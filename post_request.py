217f3ffc07d68795c3e1ae5f44cbac85e5f1c011
Congiuntivo <simone.decarli@studenti.unitn.it>
2023-04-12 19:52:56 +0200

    Now it works

--- post_request.py
+++ post_request.py
@@ -7,14 +7,18 @@ username = os.getenv('USERNAME')
 password = os.getenv('PASSWORD')
 
 session = requests.Session()
+
+# get request per ottenere i cookie
+
+session.get('https://didatticaonline.unitn.it/dol/login/index.php', allow_redirects=True)
+
+# get request per ottenere execution id
 result = session.get(
     'https://didatticaonline.unitn.it/dol/auth/shibboleth/index.php', allow_redirects=True)
 
-# print(session.url)
-location = (session.headers.get('Location'))
-jsessionid = (session.cookies.get('JSESSIONID'))
 execution = result.url.split('execution=')[1]
 
+# preparo post request per login
 
 url = 'https://idp.unitn.it/idp/profile/SAML2/Redirect/SSO?execution='+execution
 
@@ -29,7 +33,6 @@ headers = {
     "Origin": "https://idp.unitn.it",
     "Connection": "keep-alive",
     "Referer": "https://idp.unitn.it/idp/profile/SAML2/Redirect/SSO?execution="+execution,
-    "Cookie": "JSESSIONID = "+jsessionid,
     "Upgrade-Insecure-Requests": "1",
     "Sec-Fetch-Dest": "document",
     "Sec-Fetch-Mode": "navigate",
@@ -41,34 +44,22 @@ headers = {
 
 data = {'j_username': username,
         'j_password': password,
-        'dominio': '@unitn.it'
+        'dominio': '@unitn.it',
+        '_eventId_proceed': ''
         }
 
-# get set-cookie field from response (shib_idp_session)
-x = session.post(url, headers=headers, data=data, allow_redirects=False)
-cookies = session.cookies.get_dict()
-print(cookies)
+# spedisco la post request per login
 
+session.post(url, headers=headers, data=data, allow_redirects=True)
 
-# second post request to didatticaonline
-url = 'https://didatticaonline.unitn.it/Shibboleth.sso/SAML2/POST'
-headers = {
-    "User-Agent": "Mozilla/5.0",
-    "Accept": "text/html, application/xhtml+xml, application/xml;q=0.9, image/avif, image/webp, */*;q=0.8",
-    "Accept-Language": "en-US, en",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Content-Type": "application/x-www-form-urlencoded",
-    "Content-Length": "18762",
-    "Content-Type":	"application/x-www-form-urlencoded",
-    "Cookie": "ApplicationGatewayAffinityCORS =" + cookies["ApplicationGatewayAffinityCORS"] + "; ApplicationGatewayAffinity =" + cookies["ApplicationGatewayAffinity"] + "; MoodleSessiondol =" + cookies['MoodleSessiondol'],
-    "Host": "didatticaonline.unitn.it",
-    "Origin": "https://idp.unitn.it",
-    "Referer": "https://idp.unitn.it",
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "same-site",
-    "Sec-GPC": "1",
-    "TE": "trailers",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0"
-}
+# prova per vedere se ci sono tutti e 4 i cookie
+# print(session.cookies.get_dict())
+
+# prova di accesso a una pagina
+
+url = '	https://didatticaonline.unitn.it/dol/course/index.php?categoryid=682'
+
+yy = session.get(url, allow_redirects=True)
+print(yy.status_code)
+assert yy.status_code == 200
+
