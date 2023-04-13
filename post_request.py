d3595e42d22c110b25ed1388e5f4230433c46c9f
Marco <marcotoniolomail@gmail.com>
2023-04-13 15:28:49 +0200

    some changes

--- post_request.py
+++ post_request.py
@@ -8,35 +8,66 @@ password = os.getenv('PASSWORD')
 
 session = requests.Session()
 
-# get request per ottenere i cookie
-
-session.get('https://didatticaonline.unitn.it/dol/login/index.php',
-            allow_redirects=True)
+# accesso a webapp unitn
+webapp_login = session.get(
+    'https://webapps.unitn.it/gestionecorsi/', allow_redirects=True)
 
 # get request per ottenere execution id
-
 result = session.get(
     'https://didatticaonline.unitn.it/dol/auth/shibboleth/index.php', allow_redirects=True)
 
 execution = result.url.split('execution=')[1]
 
-# preparo post request per login
-
+# post request per login
 url = 'https://idp.unitn.it/idp/profile/SAML2/Redirect/SSO?execution='+execution
-
 data = {'j_username': username,
         'j_password': password,
         'dominio': '@unitn.it',
         }
-
-# spedisco la post request per login
-
 session.post(url, data=data, allow_redirects=True)
+
+# ritorno alla pagina gestione corsi
+
+
+# accesso a didattica online unitn tramite idp
+
+# get request per ottenere i cookie
+session.get('https://didatticaonline.unitn.it/dol/login/index.php',
+            allow_redirects=True)
+
+# get request per ottenere execution id
+result = session.get(
+    'https://didatticaonline.unitn.it/dol/auth/shibboleth/index.php', allow_redirects=True)
+
+execution = result.url.split('execution=')[1]
+
+# post request per login
+url = 'https://idp.unitn.it/idp/profile/SAML2/Redirect/SSO?execution='+execution
+data = {'j_username': username,
+        'j_password': password,
+        'dominio': '@unitn.it',
+        }
+session.post(url, data=data, allow_redirects=True)
+
+# link lista corsi DISI
+# url = 'https://didatticaonline.unitn.it/dol/course/index.php?categoryid=682'
+
 
 # prova di accesso a una pagina
 
-url = '	https://didatticaonline.unitn.it/dol/course/index.php?categoryid=682'
+url = 'https://didatticaonline.unitn.it/dol/enrol/index.php?id=36549'
 
 yy = session.get(url, allow_redirects=True)
-print(yy.status_code)
+
+# print(yy.text)
 assert yy.status_code == 200
+
+# tentativo di accesso a pagina  'https://webapps.unitn.it/gestionecorsi/' con post request
+url = 'https://webapps.unitn.it/gestionecorsi/'
+gestione_corsi = session.post(url, data=data, allow_redirects=True)
+
+# save html
+with open('list_file.html', 'w') as file:
+    file.write(gestione_corsi.text)
+
+print(gestione_corsi.status_code)
