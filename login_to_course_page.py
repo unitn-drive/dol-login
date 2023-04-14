3de431e1f825c970e3d6e285acceb10f3a79a5a8
Marco <marcotoniolomail@gmail.com>
2023-04-14 12:57:56 +0200

    reached any course page

--- login_to_course_page.py
+++ login_to_course_page.py
@@ -2,13 +2,33 @@ from dotenv import load_dotenv
 import requests
 import os
 from bs4 import BeautifulSoup as BS
-
+import json
 
 load_dotenv()
 username = os.getenv('username')
 password = os.getenv('password')
 session = requests.Session()
+
+
+def extract_RelayState_from_HTML(res):
+    soup = BS(res.text, 'html.parser')
+    try:
+        return soup.find('input', {'name': 'RelayState'}).get('value')
+    except:
+        print("Got unhandled exception %s" %
+              str(e) + ", while extracting RelayState from: " + res.url)
+
+
+def extract_SAMLRessponse_from_HTML(res):
+    soup = BS(res.text, 'html.parser')
+    try:
+        return soup.find('input', {'name': 'SAMLResponse'}).get('value')
+    except:
+        print("Got unhandled exception %s" %
+              str(e) + ", while extracting SAMLResponse from: " + res.url)
+
 # print("Username and password:", username, password)
+
 
 # accesso a webapp unitn
 # accesso a webapp unitn
@@ -34,15 +54,9 @@ data = {'j_username': username,
 
 res = session.post(url, data=data, allow_redirects=True)
 
-# extract SAMLResponse from last request
-soup = BS(res.text, 'html.parser')
-try:
-    tokenRelayState = soup.find('input', {'name': 'RelayState'}).get('value')
-    tokenSAMLResponse = soup.find(
-        'input', {'name': 'SAMLResponse'}).get('value')
-except Exception as e:
-    print("Got unhandled exception %s" %
-          str(e) + ", while extracting from: " + res.url)
+# extract RelayState and SAMLResponse from last request
+tokenRelayState = extract_RelayState_from_HTML(res)
+tokenSAMLResponse = extract_SAMLRessponse_from_HTML(res)
 
 
 # post to idsrv.unitn.it with tokens(Acs)
@@ -83,18 +97,54 @@ res = session.get(url, headers=headers, allow_redirects=True)
 with open('attended_courses.json', 'w') as file:
     file.write(res.text)
 
-# get aviable courses
-url = 'https://webapps.unitn.it/api/gestionecorsi/v1/studente/possibilicorsi/'
-headers = {'Authorization': 'Bearer ' + auth}
-res = session.get(url, headers=headers, allow_redirects=True)
+# # get aviable courses
+# url = 'https://webapps.unitn.it/api/gestionecorsi/v1/studente/possibilicorsi/'
+# headers = {'Authorization': 'Bearer ' + auth}
+# res = session.get(url, headers=headers, allow_redirects=True)
 
-# save json
-with open('aviable_courses.json', 'w') as file:
-    file.write(res.text)
+# # save json
+# with open('aviable_courses.json', 'w') as file:
+#     file.write(res.text)
+
+
+# output list of courses
+json = res.json()
+
+# removing multi language substring and setting url of each course
+for i in json:
+    if '{mlang other}' in i['fullName']:
+        i['fullName'] = i['fullName'].split('{mlang other}', 1)[
+            1].split('{mlang}', 1)[0]
+    i['url'] = i['urlMoodle'].split('target=', 1)[1]
+
+# visiting first course
+course = json[0]
+
+# ACTUALLY THE SECOND AUTHENTICATION
+res = session.get(course['urlMoodle'], allow_redirects=True)
+
+# extract RelayState and SAMLResponse from last request
+tokenRelayState = extract_RelayState_from_HTML(res)
+tokenSAMLResponse = extract_SAMLRessponse_from_HTML(res)
+
+# post to dol with tokens
+url = 'https://didatticaonline.unitn.it/Shibboleth.sso/SAML2/POST'
+data = {'RelayState': tokenRelayState, 'SAMLResponse': tokenSAMLResponse}
+res = session.post(url, data=data, allow_redirects=True)
+
+# should be redirected to course page
+print(res.url)
+
 
 # # save html
-# with open('response_post.html', 'w') as file:
+# with open('course_page.html', 'w') as file:
 #     file.write(res.text)
+
+# # print courses names
+# for i in json:
+#     print(i['fullName'])
+#     print()
+
 
 # # print history of requests
 # for i in res.history:
