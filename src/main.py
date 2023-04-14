8095c31c50add45266d99ab155feec927e5c2315
Marco <marcotoniolomail@gmail.com>
2023-04-14 20:21:39 +0200

    some improvement with args

--- src/main.py
+++ src/main.py
@@ -2,25 +2,43 @@ import argparse
 parser = argparse.ArgumentParser(
     description='A python program to scrape unitn courses and store resources in a local folder')
 
-parser.add_argument('username', nargs=1, type=str,
-                    help='Insert your mail or username, no format required (e.g. name.surname@unitn.it, name.surname)')
+parser.add_argument('username', nargs=1, type=str, metavar='USERNAME',
+                    help='Required: Insert your mail or username, no format required (e.g. name.surname@unitn.it, name.surname)')
 
-parser.add_argument('password', nargs=1, type=str,
-                    help='Insert your password')
+parser.add_argument('password', nargs=1, type=str, metavar='PASSWORD',
+                    help='Required: Insert your password')
 # Show courses list
-parser.add_argument('--list-enrolled', '--list-registered', '-le', dest='list-enrolled',  action=argparse.BooleanOptionalAction,
-                    help='Show list of courses you are enrolled in')
-parser.add_argument('--list-available', '--list-av', '-la', dest='list-available',
-                    action=argparse.BooleanOptionalAction, help='Show list of available courses')
-parser.add_argument('--list-all', '--all', '-a', dest='list-all',  action=argparse.BooleanOptionalAction,
-                    help='Show list of all courses')
+listgroup = parser.add_mutually_exclusive_group()
+listgroup.add_argument('--list-enrolled', '--list-registered', '-le', action=argparse.BooleanOptionalAction,
+                       help='Show list of courses you are enrolled in')
+listgroup.add_argument('--list-available', '--list-av', '-la',
+                       action=argparse.BooleanOptionalAction, help='Show list of available courses')
+listgroup.add_argument('--list-all', '--all', '-a',   action=argparse.BooleanOptionalAction,
+                       help='Show list of all courses')
 
 
 # Show course info
-parser.add_argument('--code', '--course-code', '-c', dest='code',
-                    type=int, help='Show course info, selecting by code')
-parser.add_argument('--name', '--course-name', '-n', dest='c-name',
-                    type=str, help='Show course info, selecting by name')
+coursegroup = parser.add_mutually_exclusive_group()
+coursegroup.add_argument('--code', '--course-code', '-c', action='store', metavar='COURSE_CODE',
+                         type=int, help='Show course info, selecting by code')
+coursegroup.add_argument('--course-name', '--name', '-n', action='store', metavar='COURSE_CODE',
+                         type=str, help='Show course info, selecting by name')
+
+# Download course resources (top level parser)
+# parser.add_argument()
+download_subparser = parser.add_subparsers(
+    title='Download Subcommand', help='download otion (default = download all files)', prog='download')
+
+parser_a = download_subparser.add_parser(
+    'download', help='a help')
+parser_a.add_argument('bar', type=int, help='bar help')
 
 args = parser.parse_args()
+if (args.username == None or args.password == None):
+    parser.print_help()
+    exit(1)
+if (args.list_enrolled == None and args.list_available == None):
+    args.list_enrolled = True
+    args.list_available = True
+
 print(args)
